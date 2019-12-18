f1201cc1a989f2fbc7adf6b6aefcb7b291c96be2
Ritabrata Chakraborty <ritac041@in.ibm.com>
2019-12-18 19:06:20 +0530

    camel-api

--- src/main/java/com/leanfix/camel/route/ServiceCamelRoute.java
+++ src/main/java/com/leanfix/camel/route/ServiceCamelRoute.java
@@ -1,16 +1,11 @@
 package com.leanfix.camel.route;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.http.common.HttpOperationFailedException;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.leanfix.camel.transformation.BeanService;
-import com.leanfix.data.model.Source;
 
 /**
  * 
@@ -20,11 +15,6 @@ import com.leanfix.data.model.Source;
 @Component
 public class ServiceCamelRoute extends RouteBuilder {
 
-	@Value("${rest.post.url}")
-	String RESOLVER;
-
-	@Autowired
-	BeanService beanService;
 
 	@Override
 	public void configure() throws Exception {
@@ -52,28 +42,12 @@ public class ServiceCamelRoute extends RouteBuilder {
 				.consumes("application/json")
 				.description("Transformation API")
 				.bindingMode(RestBindingMode.json)
-				.type(Source.class)
 				.to("direct:result");
 
-		//Calling Mapping resolver service to fix input and validate parameters
-		/*
-		 * from("direct:validate") .streamCaching() .log(LoggingLevel.INFO, "${body}")
-		 * .routeId("validate-source") .process(exchange -> { String jsonResponse =
-		 * (String) exchange.getIn().getBody(String.class);
-		 * exchange.getOut().setHeader(Exchange.CONTENT_TYPE, "application/json");
-		 * exchange.getOut().setBody(jsonResponse); }).setHeader(Exchange.HTTP_METHOD,
-		 * constant("POST")) .toD(RESOLVER + "?bridgeEndpoint=true")
-		 * .to("direct:getDetail");
-		 * 
-		 * //Processing validated data with mapping to respective data model
-		 * from("direct:getDetail") .routeId("data-processor") .streamCaching()
-		 * .log(LoggingLevel.INFO, "${body}") .process("beanService")
-		 * .removeHeaders("*") .to("direct:result");
-		 */
+		
 		from("direct:result")
 				.routeId("target")
 				.streamCaching()
-				.log(LoggingLevel.INFO, "${body}")
 				.setBody().constant("Success:True")
 				.log(LoggingLevel.INFO,"Completed");
 	}
